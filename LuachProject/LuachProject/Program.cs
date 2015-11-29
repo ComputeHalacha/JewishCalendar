58f926dbf7e2ad318e5f4644b4886fd99a2c0e9c
Chaim B. on ASUS-LAPTOP <none@none>
2015-11-29 10:06:07 +0200

    Added mobile app html5 project. Started converting JewishCalendar classes to javascript. Converted JewishDateMicro + JewishDateCalculations + Utils classes to LuachProjectMobileApp/www/scripts/JCal/jDate.js - (basic Jewish Date functionality seems to work OK)

--- LuachProject/LuachProject/Program.cs
+++ LuachProject/LuachProject/Program.cs
@@ -8,14 +8,15 @@ namespace LuachProject
 {
     internal static class Program
     {
-        internal static List<Location> LocationsList;
-        internal static System.Globalization.CultureInfo HebrewCultureInfo = new System.Globalization.CultureInfo("he-il");
         internal static Pen DayCellBorderPen = Pens.SteelBlue;
         internal static Brush DayHeadersBGBrush = Brushes.Lavender;
         internal static Brush DayHeadersTextBrush = Brushes.SteelBlue;
         internal static Brush DayTextBrush = Brushes.DarkBlue;
-        internal static Brush ZmanimBrush = Brushes.RoyalBlue;
+        internal static System.Globalization.CultureInfo HebrewCultureInfo = new System.Globalization.CultureInfo("he-il");
+        internal static List<Location> LocationsList;
         internal static Brush SecularDayBrush = Brushes.DarkBlue;
+        internal static Brush SelectedDayBackgroundBrush = new SolidBrush(Color.FromArgb(100, 100, 200, 255));
+        internal static Brush ShabbosBrush = Brushes.LightSteelBlue;
 
         internal static StringFormat StringFormat = new StringFormat
         {
@@ -25,9 +26,8 @@ namespace LuachProject
             FormatFlags = StringFormatFlags.LineLimit | StringFormatFlags.FitBlackBox
         };
 
-        internal static Brush ShabbosBrush = Brushes.LightSteelBlue;
-        internal static Brush SelectedDayBackgroundBrush = new SolidBrush(Color.FromArgb(100, 100, 200, 255));
         internal static Brush YomtovBrush = Brushes.GhostWhite;
+        internal static Brush ZmanimBrush = Brushes.RoyalBlue;
 
         static Program()
         {
@@ -37,79 +37,6 @@ namespace LuachProject
                 Properties.Settings.Default.Save();
             }
         }
-
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        private static void Main(string[] args)
-        {
-            if (Properties.Settings.Default.NeedsUpdate)
-            {
-                Properties.Settings.Default.Upgrade();
-                Properties.Settings.Default.NeedsUpdate = false;
-            }
-
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            LoadLocations();
-            Form form = null;
-            if (args.Length > 0)
-            {
-                switch (args[0])
-                {
-                    case "English":
-                        form = new frmMonthlyEnglish();
-                        break;
-
-                    case "Hebrew":
-                        form = new frmMonthlyHebrew();
-                        break;
-
-                    case "SecularEnglish":
-                        form = new frmMonthlySecular();
-                        break;
-
-                    case "SecularHebrew":
-                        form = new frmMonthlySecular { DisplayHebrew = true };
-                        break;
-                }
-            }
-            else if (!string.IsNullOrEmpty(Properties.Settings.Default.LastLanguage))
-            {
-                switch (Properties.Settings.Default.LastLanguage)
-                {
-                    case "English":
-                        form = new frmMonthlyEnglish();
-                        break;
-
-                    case "Hebrew":
-                        form = new frmMonthlyHebrew();
-                        break;
-
-                    case "SecularEnglish":
-                        form = new frmMonthlySecular();
-                        break;
-
-                    case "SecularHebrew":
-                        form = new frmMonthlySecular { DisplayHebrew = true };
-                        break;
-                }
-            }
-            Application.Run(form ?? new frmMonthlyEnglish());
-        }
-
-        /// <summary>
-        /// Loads the locations from the settings.
-        /* Note: the xml format for each locations is:
-            <L N="Ofakim" H="אופקים" I="Y">    <!--N = name of location, H = hebrew name (optional), I = is the location in Israel? [Y = yes] (optional)-->
-                <T>2</T>    <!--Time zone: hour offset from UTC (AKA GMT) -->
-                <E>170</E>    <!--Elevation in meters (optional)-->
-                <LT>31.32</LT>    <!--Latitude-->
-                <LN>-34.62</LN>   <!--Longitude-->
-                <CL>30</CL>    <!--Candle-lighting: minutes before sunset (optional)-->
-                <TZN>Israel Standard Time</TZN>    <!--Time zone name (optional)-->
-            </L>     */
 
         /// </summary>
         public static void LoadLocations()
@@ -174,5 +101,81 @@ namespace LuachProject
                 ms.Close();
             }
         }
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main(string[] args)
+        {
+            if (Properties.Settings.Default.NeedsUpdate)
+            {
+                Properties.Settings.Default.Upgrade();
+                Properties.Settings.Default.NeedsUpdate = false;
+            }
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            LoadLocations();
+            Form form = null;
+            if (args.Length > 0)
+            {
+                switch (args[0])
+                {
+                    case "English":
+                        form = new frmMonthlyEnglish();
+                        break;
+
+                    case "Hebrew":
+                        form = new frmMonthlyHebrew();
+                        break;
+
+                    case "SecularEnglish":
+                        form = new frmMonthlySecular();
+                        break;
+
+                    case "SecularHebrew":
+                        form = new frmMonthlySecular { DisplayHebrew = true };
+                        break;
+                }
+            }
+            else if (!string.IsNullOrEmpty(Properties.Settings.Default.LastLanguage))
+            {
+                switch (Properties.Settings.Default.LastLanguage)
+                {
+                    case "English":
+                        form = new frmMonthlyEnglish();
+                        break;
+
+                    case "Hebrew":
+                        form = new frmMonthlyHebrew();
+                        break;
+
+                    case "SecularEnglish":
+                        form = new frmMonthlySecular();
+                        break;
+
+                    case "SecularHebrew":
+                        form = new frmMonthlySecular { DisplayHebrew = true };
+                        break;
+                }
+            }
+
+            JewishDateMicro jd = new JewishDateMicro();
+            var ad = (jd.AbsoluteDate.ToString());
+            Application.Run(form ?? new frmMonthlyEnglish());
+        }
+
+        /// <summary>
+        /// Loads the locations from the settings.
+        /* Note: the xml format for each locations is:
+            <L N="Ofakim" H="אופקים" I="Y">    <!--N = name of location, H = hebrew name (optional), I = is the location in Israel? [Y = yes] (optional)-->
+                <T>2</T>    <!--Time zone: hour offset from UTC (AKA GMT) -->
+                <E>170</E>    <!--Elevation in meters (optional)-->
+                <LT>31.32</LT>    <!--Latitude-->
+                <LN>-34.62</LN>   <!--Longitude-->
+                <CL>30</CL>    <!--Candle-lighting: minutes before sunset (optional)-->
+                <TZN>Israel Standard Time</TZN>    <!--Time zone name (optional)-->
+            </L>     */
     }
 }
