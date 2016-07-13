4da9cbb4987c7f0a8597cce1490f177a7a9f191b
CB Sommers - Laptop <cb@compute.co.il>
2016-07-13 13:56:53 +0300

    Formatted date in Luach.py

--- LuachPython/Luach.py
+++ LuachPython/Luach.py
@@ -16,27 +16,24 @@ def display(title, value):
             else:
                 print('{:.<30} {}'.format(title, str(value)))
         except TypeError:
-            print('oink')
-
+            print('{}.........{}'.format(title, value))
 
 if __name__ == '__main__':
     cityNameStart = 'Modi'.lower()
     file = open('Files/LocationsList.json', 'r', encoding='utf-8')
     b = load(file)
     mi = [Location.parse(m) for m in b['locations'] if m['n'].lower().startswith(cityNameStart)]
-
     if len(mi):
         lo = mi[0]
         jd = JewishDate.JewishDate.today()
         nextweek = jd.addDays(7)
-
         while jd < nextweek:
+            print('\n--{:-<50}'.format(jd.todate().strftime('%A, %B %d, %Y')))
             infos = LuachPython.JewishCalendar.getDailyInfo(jd, lo, HEBREW)
             dz = LuachPython.JewishCalendar.getDailyZmanim(jd, lo, HEBREW)
             for i, v in infos.items():
                 display(i, v)
             for i, v in dz.items():
                 display(i, v)
-
             jd = jd.addDays(1)
-            print('\n-----------------------------------------------------------------------\n')
+
