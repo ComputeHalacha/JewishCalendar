c666f6a828ebe1c429dfc1cac919c8d1ef465ef7
CB Sommers - Laptop <cb@compute.co.il>
2016-07-15 17:56:03 +0300

    More refactoring

--- pyluach/luach.py
+++ pyluach/luach.py
@@ -6,7 +6,7 @@ from jcal.jdate import JDate
 from jcal.location import Location
 
 HEBREW = False
-ARMYTIME = False
+ARMY_TIME = False
 
 
 def display(title, value):
@@ -15,7 +15,7 @@ def display(title, value):
     else:
         try:
             if isinstance(value, HourMinute):
-                value = value.tostring(ARMYTIME)
+                value = value.tostring(ARMY_TIME)
             if HEBREW:
                 print('{:.<25}{:.>25}'.format(title, str(value)))
             else:
@@ -24,15 +24,15 @@ def display(title, value):
             print('{}.........{}'.format(title, value))
 
 
-def display_week(startJd, nameSearch):
+def display_week(startjd, namesearch):
     file = open('Files/LocationsList.json', 'r', encoding='utf-8')
     b = json.load(file)
     loc_raw = next(iter([m for m in b['locations']
-                         if nameSearch.lower() in m['n'].lower()]), None)
+                         if namesearch.lower() in m['n'].lower()]), None)
     if loc_raw:
         location = Location.parse(loc_raw)
-        print('** ZMANIM FOR WEEK STARTING {} IN {} {:*<15}'.format(startJd, location.name.upper(), ''))
-        jd = startJd
+        print('** ZMANIM FOR WEEK STARTING {} IN {} {:*<15}'.format(startjd, location.name.upper(), ''))
+        jd = startjd
         nextweek = jd + 7
         while jd < nextweek:
             print('\n--{:-<50}'.format(jd.todate().strftime('%A, %B %d, %Y')))
@@ -44,7 +44,7 @@ def display_week(startJd, nameSearch):
                 display(i, v)
             jd += 1
     else:
-        print('No location found that matches with "%s"' % (nameSearch))
+        print('No location found that matches with "%s"' % (namesearch))
 
 
 if __name__ == '__main__':
