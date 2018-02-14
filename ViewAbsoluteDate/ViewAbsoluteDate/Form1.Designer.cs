89a5ba14f8ba3f490e7034717f038b0288f84a56
Chaim B. Sommers <cb@charity-choice.org>
2018-02-14 19:07:47 +0200

    Added ViewAbsoluteDate

---
+++ ViewAbsoluteDate/ViewAbsoluteDate/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace ZmanimAnywhere
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.jdpFrom = new JewishDatePicker.JewishDatePicker();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // jdpFrom
+            // 
+            this.jdpFrom.AutoSize = true;
+            this.jdpFrom.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.jdpFrom.BackColor = System.Drawing.Color.White;
+            this.jdpFrom.Font = new System.Drawing.Font("Tahoma", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.jdpFrom.ForeColor = System.Drawing.Color.Black;
+            this.jdpFrom.Location = new System.Drawing.Point(18, 32);
+            this.jdpFrom.Margin = new System.Windows.Forms.Padding(0);
+            this.jdpFrom.MaxDate = ((JewishCalendar.JewishDate)(resources.GetObject("jdpFrom.MaxDate")));
+            this.jdpFrom.MinDate = ((JewishCalendar.JewishDate)(resources.GetObject("jdpFrom.MinDate")));
+            this.jdpFrom.Name = "jdpFrom";
+            this.jdpFrom.Size = new System.Drawing.Size(245, 24);
+            this.jdpFrom.TabIndex = 27;
+            this.jdpFrom.Value = ((JewishCalendar.JewishDate)(resources.GetObject("jdpFrom.Value")));
+            this.jdpFrom.ValueChanged += new System.EventHandler(this.jdpFrom_ValueChanged);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(18, 16);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(67, 14);
+            this.label11.TabIndex = 30;
+            this.label11.Text = "Jewish Date";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 14);
+            this.label2.TabIndex = 31;
+            this.label2.Text = "Secular Date";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(18, 94);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(242, 20);
+            this.dateTimePicker1.TabIndex = 32;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(18, 134);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(242, 279);
+            this.tableLayoutPanel1.TabIndex = 33;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(290, 440);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.jdpFrom);
+            this.Font = new System.Drawing.Font("Verdana", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Zmanim Anywhere";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private JewishDatePicker.JewishDatePicker jdpFrom;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+    }
+}
+
---
+++ ViewAbsoluteDate/ViewAbsoluteDate/Form1.cs
@@ -0,0 +1,52 @@
+﻿using JewishCalendar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ZmanimAnywhere
+{
+    public partial class Form1 : Form
+    {      
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (SystemInformation.TerminalServerSession)
+                return;
+
+            PropertyInfo aProp =
+                  typeof(Control).GetProperty(
+                        "DoubleBuffered",
+                        BindingFlags.NonPublic |
+                        BindingFlags.Instance);
+
+            aProp.SetValue(this, true, null);
+            this.jdpFrom.Value = new JewishDate();            
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+        }
+        
+        
+        private void jdpFrom_ValueChanged(object sender, EventArgs e)
+        {
+            var jd = this.jdpFrom.Value;
+            this.dateTimePicker1.Value = jd.GregorianDate;
+
+            this.tableLayoutPanel1.Controls.Add(new Label { Text = jd.AbsoluteDate.ToString() });
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {            
+            this.jdpFrom.Value = new JewishDate(this.dateTimePicker1.Value);
+        }
+    }
+}
